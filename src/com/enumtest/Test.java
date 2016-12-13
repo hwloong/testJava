d2d68fab891bc51c516dff4fbddfdfad6ce61bab
hwloong <hwloong@126.com>
2016-12-13 15:13:04 +0800

    java枚举

---
+++ src/com/enumtest/Test.java
@@ -0,0 +1,8 @@
+package com.enumtest;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Typetest.Tt.getS1();
+	}
+}
---
+++ src/com/enumtest/Typetest.java
@@ -0,0 +1,36 @@
+package com.enumtest;
+
+public enum Typetest {
+	
+	Tt("1","a");
+
+	private String s1;
+	
+	private String s2;
+	
+	
+	Typetest(String s1,String s2){
+		this.s1 = s1;
+		this.s2 = s2;
+	}
+
+
+	public String getS1() {
+		return s1;
+	}
+
+
+	public void setS1(String s1) {
+		this.s1 = s1;
+	}
+
+
+	public String getS2() {
+		return s2;
+	}
+
+
+	public void setS2(String s2) {
+		this.s2 = s2;
+	}
+}
